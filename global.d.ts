4e7570995e58327e2e8055f9a5557704eb807542
LostOne <48584533+Lost1ck@users.noreply.github.com>
2024-07-11 23:35:55 +0200

    fix

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,4 @@
 declare module '*.module.css' {
-  const content: { [className: string]: string };
-  export default content;
+  const classes: { [key: string]: string };
+  export default classes;
 }
